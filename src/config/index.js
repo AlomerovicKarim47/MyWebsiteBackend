f3004fd8c9396982ab7fdc8990f1b8875eb6342e
alomerovickarim47 <karimalomerovic@yahoo.com>
2023-04-20 23:40:08 +0200

    tweaking env

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,7 @@ dotenv.config()
 const config = {
     ALLOWED_ORIGIN: process.env.NODE_ENV==="development"
         ?"http://localhost:3000"
-        :"https://akdev.onrender.com/",
+        :"https://akdev.onrender.com",
     PORT: 4000,
     EMAIL: process.env.EMAIL,
     EMAIL_PASSWORD: process.env.EMAIL_PASSWORD
